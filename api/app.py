51af2eee338e4cc13f77411bc45b812ad179bac9
dani-doni <dani.doni74@gmail.com>
2023-09-18 12:54:12 +0200

    without database connection

--- api/app.py
+++ api/app.py
@@ -1,18 +1,6 @@
 from flask import Flask, render_template
 import json, os
 
-path_to_json = './database/'
-json_files = os.listdir(path_to_json)
-results_merged = []
-
-for i in range(0,len(json_files)):
-    file = json_files[i]
-    with open(os.path.join(path_to_json, file)) as f:
-       data_json = json.load(f)
-       data = data_json['data']
-       results = data['result']
-       results_merged = results_merged + results
-
 app = Flask(__name__)
 
 @app.route('/')
